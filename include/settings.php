a3121dd4aa3c32755ea0797949f92243681a4ac5
Fuad1610006 <afmfuad96@gmail.com>
2023-05-23 12:42:04 +0600

    first commit

---
+++ include/settings.php
@@ -0,0 +1,5 @@
+<?php
+    $base_url="http://localhost/ems/";
+    require_once('class/crud.php');
+    $mysqli=new crud;
+?>
---
+++ login.php
@@ -0,0 +1,88 @@
+<?php require_once('include/settings.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - SB Admin</title>
+        <link href="<?= $base_url?>assets/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-body">
+                                        <form action="" method="post">
+                                            <div class="form-floating mb-3">
+                                                <input name="email" class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input name="password" class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="#">Forgot Password?</a>
+                                                <button type="submit" class="btn btn-primary">Login</button>
+                                            </div>
+                                        </form>
+                                        <?php
+                                            if($_POST){
+                                            $where['email']=$_POST['email'];
+                                            $where['password']=sha1(md5($_POST['password']));
+                                            
+                                            $rs=$mysqli->common_select('tbl_users','*',$where);
+                                            if(!$rs['error']){
+                                                session_start();
+                                                if(isset($rs['data'][0])){
+                                                $_SESSION['userid']=$rs['data'][0]->id;
+                                                $_SESSION['username']=$rs['data'][0]->name;
+                                                $_SESSION['contact']=$rs['data'][0]->contact_no;
+                                                $_SESSION['email']=$rs['data'][0]->email;
+                                                $_SESSION['image']=$rs['data'][0]->image;
+                                                }
+                                                echo "<script>window.location='index.php'</script>";
+                                            }else{
+                                                echo $rs['error'];
+                                            }
+                                            }
+
+                                        ?>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="register.php">Need an account? Sign up!</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?= $base_url?>assets/js/scripts.js"></script>
+    </body>
+</html>
