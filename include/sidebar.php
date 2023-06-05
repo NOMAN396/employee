3ce9b2ed42aa9e331dd801cfd9c9275d94a12363
Burhan Uddin Fuad <132288323+Fuad1610006@users.noreply.github.com>
2023-06-05 19:31:43 +0600

    added payslip

--- include/sidebar.php
+++ include/sidebar.php
@@ -38,8 +38,8 @@
 					<div class="collapse" id="colhr" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
 						<nav class="sb-sidenav-menu-nested nav">
 							<a class="nav-link" href="<?= $base_url?>employee_salary_list.php">Employee salary</a>
-							<a class="nav-link" href="layout-sidenav-light.html">Payslip</a>
-							<a class="nav-link" href="<?= $base_url?>payroll_additions.php">Payroll Items</a>
+							<a class="nav-link" href="employee_payslip.php">Payslip</a>
+							<a class="nav-link" href="<?= $base_url?>employee_payroll_items.php">Payroll Items</a>
 						</nav>
 					</div><br>
 					
