eca1eeda0b34bcb6b2c0031bd1c624f19f5e0d8a
shariful Islam <ether_sb@yahoo.com>
2023-06-21 12:43:02 +0600

    salary done

--- employee_salary_list.php
+++ employee_salary_list.php
@@ -9,20 +9,22 @@
     <table class="table">
         <tr>
             <th>#</th>
-            <th>Employee</th>
-            <th>Employee_ID</th>
-            <th>Email</th>
-            <th>Join Date</th>
-            <th>Role</th>
-            <th>Salary</th>
+            <th>Basic</th>
             <th>Payslip</th>
             <th>Action</th>
         </tr>
         <?php
-                  $data=$mysqli->common_select_query("SELECT employee_salary.*,tbl_employees.first_name,tbl_employees.last_name,tbl_employees.employee_id,tbl_employees.email,tbl_employees.joining_date
-                  FROM `employee_salary`
-                  join tbl_employees on tbl_employees.id=employee_salary.id Where employee_salary.deleted_at is null
-                  ");
+                  $data=$mysqli->common_select_query("SELECT 
+                  sum(`basic`) as basic,
+                  sum(`house_rent`) as house_rent,
+                  sum(`medical_allowance`) as medical_allowance,
+                  sum(`bonus`) as bonus,
+                  sum(`provident_fund`) as provident_fund,
+                  sum(`leave_deduction`) as leave_deduction,
+                  sum(`tax`) as tax,
+                  sum(`total`) as total, s_year,s_month
+                  FROM `tbl_salary`
+                  group by `s_year`,`s_month`");
                if(!$data['error']){
                     foreach($data['data'] as $d){
                 ?>
@@ -35,19 +37,9 @@
             <td><?= $d->designation ?></td>
             <td><?= $d->salary ?></td>
             <td><?= $d->payslip ?></td>
-            <td>
-                <a title="Update" href="employee_salary_edit.php?id=<?= $d->id ?>">
-                            <i class="fa fa-edit"></i>
-                </a>
-                <a title="Delete" class="text-danger" href="employee_salary_delete.php?id=<?= $d->id ?>">
-                            <i class="fa fa-trash"></i>
-                </a>
-            </td>
+            
         </tr>
-        <?php
-        }
-                  }
-                  ?>
+        <?php } } ?>
         
     </table>
 </div>
