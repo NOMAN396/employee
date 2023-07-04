71e18bc92447c496dd7168d7aef6ad9fc60b8fb7
jasim1205 <jasimuddinm180@gmail.com>
2023-07-04 12:45:24 +0600

    login update

--- login.php
+++ login.php
@@ -60,7 +60,6 @@
                                     </div>
                                     <div class="card-footer text-center py-3">
                                         <div class="small"><a href="register.php">Need an account? Sign up!</a></div>
-                                        <div class="small"><a href="employee_login.php">Employee Login</a></div>
                                     </div>
                                 </div>
                             </div>
